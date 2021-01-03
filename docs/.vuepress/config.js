1eb169ff104c0c35ae6f9fb6853b8add24d40f8b
Rob A <classicvwnetwork@gmail.com>
2021-01-03 00:44:51 -0500

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Smart Contract for a ERC20 Token',
+  description: 'A simple Smart Contract for a Standard, Capped, Mintable, Burnable, Payable ERC20 Token.',
+  base: '/erc20-token/',
+  plugins: [
+    ['@vuepress/google-analytics', {
+      ga: 'UA-115756440-2',
+    }],
+  ],
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+  ],
+  themeConfig: {
+    repo: 'vittominacori/erc20-token',
+    sidebar: 'auto',
+  },
+};
